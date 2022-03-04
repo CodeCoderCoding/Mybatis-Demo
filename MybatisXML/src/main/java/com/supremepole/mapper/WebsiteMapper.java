17160bb0f665067d35bb7a0c983d04f16266cd2f
CodeCoderCoding <1845371176@qq.com>
2022-03-04 13:58:26 +0800

    update mybatisXML

---
+++ MybatisXML/src/main/java/com/supremepole/mapper/WebsiteMapper.java
@@ -0,0 +1,10 @@
+package com.supremepole.mapper;
+
+import com.supremepole.model.Website;
+
+/**
+ * @author CodeCoderCoding
+ */
+public interface WebsiteMapper {
+    Website selectById(Integer id);
+}
---
+++ MybatisXML/src/test/java/com/supremepole/mapper/WebsiteMapperTest.java
@@ -0,0 +1,42 @@
+package com.supremepole.mapper;
+
+import java.io.IOException;
+import java.io.Reader;
+
+import com.supremepole.model.Website;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author CodeCoderCoding
+ */
+public class WebsiteMapperTest {
+    private static SqlSessionFactory sqlSessionFactory;
+
+    @BeforeClass
+    public static void init(){
+        try {
+            Reader reader = Resources.getResourceAsReader("mybatis-config.xml");
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+            reader.close();
+        } catch (IOException ignore) {
+            ignore.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testSelectAll(){
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        try {
+            WebsiteMapper websiteMapper = sqlSession.getMapper(WebsiteMapper.class);
+            Website website=websiteMapper.selectById(1);
+            System.out.println(website);
+        } finally {
+            sqlSession.close();
+        }
+    }
+}
